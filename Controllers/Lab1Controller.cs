0276e5574a43a134b655e39177428f3ae4623d73
ivanm151 <ivmedv5@gmail.com>
2025-01-20 14:12:26 +0700

    Lab1: 1st and 2nd view methods added

---
+++ Controllers/Lab1Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Programming.Controllers
+{
+
+    public class Lab1Controller : Controller
+    {
+        public IActionResult FirstViewMethod()
+        {
+            List<string> fruits = GetItemsList();
+            return View(fruits);
+        }
+        public IActionResult SecondViewMethod()
+        {
+            return View(GetItemsList().OrderBy(x => x).ToList());
+        }
+
+        public List<string> GetItemsList()
+        {
+            List<string> fruits = new List<string>();
+
+            fruits.Add("Apple");
+            fruits.Add("Banana");
+            fruits.Add("Peach");
+            fruits.Add("Pear");
+            fruits.Add("Pineapple");
+
+            return fruits;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Lab1}/{action=SecondViewMethod}/{id?}");
 
 app.Run();
